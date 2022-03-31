cc34376ef032cf7aba6e74976356ced95ab2fedf
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-03-31 16:30:39 +0530

    finished

--- me/karunarathne/CO1212/ArrayOperations/OperableArray.java
+++ me/karunarathne/CO1212/ArrayOperations/OperableArray.java
@@ -16,46 +16,67 @@ public class OperableArray implements Operations {
     // Implement the method to display the numbers of the array
     public void printArray() {
         for (int i=0; i<length; i++) {
-            System.out.print (baseArray [i] + " "); ;
+            System.out.print (baseArray [i] + " ");
         }
         System.out.println () ;
     }
 
     // Implement the method to display the numbers reversely
     public void printReverse() {
-        for (int i=length-1; i>=0; i++) {
-            System.out.print (baseArray [i] + " "); ;
+        for (int i=length-1; i>=0; i--) {
+            System.out.print (baseArray [i] + " ");
         }
         System.out.println () ;
     }
 
     // Implement the method to search a specific number from the array
-    public static void Search() {
-
+    public int Search(int number) {
+        for (int i=0; i<length; i++) {
+            if (baseArray [i] == number) return i;
+        }
+        return -1 ;
     }
 
     // Implement the method to find the maximum number from the array
-    public static void Maximum() {
-
+    public int Maximum() {
+        int max = baseArray [0] ;
+        for (int i=0; i<length; i++) {
+            if (baseArray [i] > max) max = baseArray [i] ;
+        }
+        return max ;
     }
 
     // Implement the method to find the minimum number from the array
-    public static void Minimum() {
-
+    public int Minimum() {
+        int min = baseArray [0] ;
+        for (int i=0; i<length; i++) {
+            if (baseArray [i] < min) min = baseArray [i] ;
+        }
+        return min ;
     }
 
     // Implement the method to retrieve a number at specific index
-    public static void getElement() {
-
+    public int getElement(int index) {
+       return baseArray [index] ;
     }
 
     // Implement the method to replace a number at specific index
-    public static void replaceElement() {
-
+    public void replaceElement(int index, int replaceWith) {
+        baseArray [index] = replaceWith ;
     }
 
     // Implement the method to sort the array in ascending order
-    public static void sortArrayASC() {
-
+    public void sortArrayASC() {
+        int [] sortedArray = new int [length] ;
+        for (int i=0; i<length; i++) {
+            int currentMin = baseArray [i] ;
+            for (int j=i+1; j<length; j++) {
+                if (baseArray [j] < currentMin) {
+                    currentMin = baseArray [j] ;
+                }
+            }
+            sortedArray [i] = currentMin ;
+        }
+        baseArray = sortedArray ;
     }
 }
