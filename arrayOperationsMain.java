9e8295fb1ad49e94b45edd2539b8f824e2e10f86
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-03-31 16:03:42 +0530

    wrote tests (main method)

--- arrayOperationsMain.java
+++ arrayOperationsMain.java
@@ -3,13 +3,31 @@ import java.util.Scanner;
 class arrayOperationsMain {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		arrayOperations op = new arrayOperations();
+		arrayOperations op = new arrayOperations(); // useless because all methods are static
 		
 		System.out.println("Enter the size of the array");
 		int size = sc.nextInt();
 		
 		int [] array = new int[size];
 
-		// Call the methods here......
+		arrayOperations.fillArray(array) ;
+
+		arrayOperations.printArray(array) ;
+
+		arrayOperations.printReverse(array) ;
+
+		arrayOperations.Search(array, 5) ;
+
+		arrayOperations.Maximum(array) ;
+
+		arrayOperations.Minimum(array) ;
+
+		arrayOperations.getElement(array, 2) ;
+
+		arrayOperations.replaceElement(array, 3, 44) ;
+
+		arrayOperations.sortArrayASC(array) ;
+
+		arrayOperations.Delete(array) ;
 	}
 }
