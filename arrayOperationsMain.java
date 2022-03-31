2ca080d5889ba2060667b94597c67847b5debf2e
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-03-31 15:11:36 +0530

    Init

--- arrayOperationsMain.java
+++ arrayOperationsMain.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
-class arrayOperationsMain
-{
-	public static void main(String args[])
-	{
+
+class arrayOperationsMain {
+	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		arrayOperations op = new arrayOperations();
 		
---
+++ me/karunarathne/CO1212/ArrayOperations/ArrayOperations.java
@@ -0,0 +1,53 @@
+package me.karunarathne.CO1212.ArrayOperations;
+
+public class ArrayOperations implements Operations {
+    // Implement the method to insert the numbers into the array
+    // (Get the numbers from the user.)
+    public static void fillArray() {
+
+    }
+
+    // Implement the method to display the numbers of the array
+    public static void printArray() {
+
+    }
+
+    // Implement the method to display the numbers reversely
+    public static void printReverse() {
+
+    }
+
+    // Implement the method to search a specific number from the array
+    public static void Search() {
+
+    }
+
+    // Implement the method to find the maximum number from the array
+    public static void Maximum() {
+
+    }
+
+    // Implement the method to find the minimum number from the array
+    public static void Minimum() {
+
+    }
+
+    // Implement the method to retrieve a number at specific index
+    public static void getElement() {
+
+    }
+
+    // Implement the method to replace a number at specific index
+    public static void replaceElement() {
+
+    }
+
+    // Implement the method to sort the array in ascending order
+    public static void sortArrayASC() {
+
+    }
+
+    public static void Delete() {
+
+    }
+}
