3a9824af472abe3c9f18e7e693408ab4ccfdee21
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-03-31 14:48:09 +0530

    Added Questions

---
+++ arrayOperations.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+class arrayOperations
+{
+	// Implement the method to insert the numbers into the array (Get the numbers from the user.)
+	public static void fillArray()
+	{
+		// Code here.....
+	}
+	
+	// Implement the method to dispaly the numbers of the array
+	public static void printArray()
+	{
+		// Code here.....
+	}
+	
+	// Implement the method to dispaly the numbers reversely
+	public static void printReverse()
+	{
+		// Code here.....
+	}
+	
+	// Implement the method to search a specific number from the array
+	public static void Search()
+	{
+		// Code here.....
+	}
+	
+	// Implement the method to find the maximum number from the array
+	public static void Maximum()
+	{
+		// Code here.....
+	}
+	
+	// Implement the method to find the minimum number from the array
+	public static void Minimum()
+	{
+		// Code here.....
+	}
+	
+	// Implement the method to retrieve a number at specific index
+	public static void getElement()
+	{
+		// Code here.....
+	}
+	
+	// Implement the method to replace a number at specific index
+	public static void replaceElement()
+	{
+		// Code here.....
+	}
+	
+	// Implement the method to sort the array in ascending order
+	public static void sortArrayASC()
+	{
+		// Code here.....
+	}
+	
+	public static void Delete()
+	{
+		// Code here.....
+	}
+}
---
+++ arrayOperationsMain.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class arrayOperationsMain
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		arrayOperations op = new arrayOperations();
+		
+		System.out.println("Enter the size of the array");
+		int size = sc.nextInt();
+		
+		int array[] = new int[size];
+		
+		// Call the methods here......
+	}
+}
